423e929b98c8a274dc87fcc35ee1ce198a673aa6
Luiz Philippe <luizphilippep@gmail.com>
2021-08-07 19:24:36 -0300

    Extracted get charge method into Movie class
    Commit 9

--- blockbuster/src/Rental.java
+++ blockbuster/src/Rental.java
@@ -17,23 +17,7 @@ public class Rental {
     }
 
     public double getCharge() {
-        double amount = 0;
-        switch (this.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                amount += 2;
-                if (this.getDaysRented() > 2)
-                    amount += (this.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                amount += this.getDaysRented() * 3;
-                break;
-            case Movie.CHILDREN:
-                amount += 1.5;
-                if (this.getDaysRented() > 3)
-                    amount += (this.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return amount;
+        return this.getMovie().getCharge(this.getDaysRented());
     }
 
     public int getFrequentRenterPoints(){
