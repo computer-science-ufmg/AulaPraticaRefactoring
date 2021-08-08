e4c611e64fd92b17d79be441eaebabd14d428df8
Luiz Philippe <luizphilippep@gmail.com>
2021-08-07 21:41:45 -0300

    Extracted getCharge into Price class

--- blockbuster/src/Movie/Price/Price.java
+++ blockbuster/src/Movie/Price/Price.java
@@ -1,5 +1,27 @@
 package Movie.Price;
 
+import Movie.Movie;
+
 public abstract class Price {
     public abstract int getPriceCode();
+
+    public double getCharge(int daysRented){
+        double amount = 0;
+        switch (this.getPriceCode()) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (daysRented > 2)
+                    amount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                amount += daysRented * 3;
+                break;
+            case Movie.CHILDREN:
+                amount += 1.5;
+                if (daysRented > 3)
+                    amount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return amount;
+    }
 }
