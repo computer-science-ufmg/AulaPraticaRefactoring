0b4c3a729d839759f5d8f10a467c9e800ee67def
Luiz Philippe <luizphilippep@gmail.com>
2021-08-07 21:54:07 -0300

    Specification of getFrequentRenterPoints in new release price class
    Commit 15

--- blockbuster/src/Movie/Price/Price.java
+++ blockbuster/src/Movie/Price/Price.java
@@ -8,14 +8,7 @@ public abstract class Price {
     public abstract double getCharge(int daysRented);
 
     public int getFrequentRenterPoints(int daysRented) {
-        // add frequent renter points
-        int points = 1;
-        // add bonus for a two day new release rental
-        if ((this.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1){
-            points ++;
-        }
-
-        return points;
+        return 1;
     }
 
 }
